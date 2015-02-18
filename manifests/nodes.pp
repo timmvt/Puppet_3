9ea933dc957f24f6e69f5389457f35f3b3152d9c
timmvt <timmvt@gmail.com>
2015-02-18 17:29:13 -0500

    Updating nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -54,25 +54,41 @@ node 'puppetDemo' {
   include sudoers
 
 # PAGE 80 - step 4
-#  include ssh
+  include ssh
+
+# PAGE 73
+  user { 'art':
+    ensure => present,
+    comment => 'Art Vandalay',
+    home => '/home/art',
+    managehome => true,
+  }
 
 # PAGE 77
-  #  ssh_authorized_key { 'art_ssh':
-  #    user => 'art',
-  #    type => 'rsa',
-  #    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
-  #  }
+  ssh_authorized_key { 'art_ssh':
+    user => 'art',
+    type => 'rsa',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
+  }
+
+# PAGE 73
+  user { 'testProfile':
+    ensure => present,
+    comment => 'test profile',
+    home => '/home/test',
+    managehome => true,
+  }
+
+# PAGE 77
+  ssh_authorized_key { 'testProfile_ssh':
+    user => 'testProfile',
+    type => 'rsa',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
+  }
+
 
 # PAGE 74
   #  ensure      => absent,
-  #  }
-
-# PAGE 73
-  #  user { 'art':
-  #    ensure => present,
-  #    comment => 'Art Vandalay',
-  #    home => '/home/art',
-  #    managehome => true,
   #  }
 
 # PAGE 65 - Yet another refactoring demonstration
