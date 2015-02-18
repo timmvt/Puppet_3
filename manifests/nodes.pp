65e8540249305040d8c96b5155f0ade631d6403e
timmvt <timmvt@gmail.com>
2015-02-18 14:40:41 -0500

    More cleanup of manifests/nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -14,6 +14,7 @@ node 'puppetDemo' {
     recurse => true,
     require => File['/var/www/cat-pictures'],
   }
+
 # PAGE 94
  #CHAPTER 6 EXERCISE
    cron { 'run pull-updates from Git':
@@ -36,19 +37,16 @@ node 'puppetDemo' {
     minute  => '00',
   }
 
-# Declares user
-#  user { 'art':
-    # Ensures user is present
-#    ensure     => present,
-    # Specifics about user
-#    comment    => 'Art Vandalay',
-    # Sets the path to user's home directory
-#    home       => '/home/art',
-    # Home directory not created without the following line
-#    managehome => true,
-# PAGE 74
-    # ensure      => absent,
+# PAGE 88
+#  exec { 'Run my arbitrary command':
+#    command => '/bin/echo I ran this command on `/bin/date` >/tmp/command.output.txt',
 #  }
+
+# PAGE 82 - step 6
+ #  include sudoers
+
+# PAGE 80 - step 4
+ #  include ssh
 
 # PAGE 77
 #  ssh_authorized_key { 'art_ssh':
@@ -57,15 +55,8 @@ node 'puppetDemo' {
 #    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
 #  }
 
-# PAGE 88
-#  exec { 'Run my arbitrary command':
-#    command => '/bin/echo I ran this command on `/bin/date` >/tmp/command.output.txt',
-#  }
-
-
-# PAGE 34
-#  package { 'nginx':
-#    ensure => installed,
+# PAGE 74
+#  ensure      => absent,
 #  }
 
 # PAGE 73
@@ -76,21 +67,23 @@ node 'puppetDemo' {
 #    managehome => true,
 #  }
 
-# PAGE 39 - This is a demonstration of refactoring
-#  include nginx
-#}
-
-# PAGE 55
-#node 'puppetDemo2' {
-  
 # PAGE 65 - Yet another refactoring demonstration
 #  include nginx
 #  }
 
-# PAGE 80 - step 4
-#  include ssh
+# PAGE 55
+#node 'puppetDemo2' {
 
-# PAGE 82 - step 6
-#  include sudoers
+# PAGE 39 - This is a demonstration of refactoring
+#  include nginx
+#}
+
+# PAGE 34
+#  package { 'nginx':
+#    ensure => installed,
+#  }
+
+  
+
   
 }
