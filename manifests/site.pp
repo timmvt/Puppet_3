abcb41af47ee845a7ef75343f3ab7970310c64e8
timmvt <timmvt@gmail.com>
2015-02-20 09:31:38 -0500

    Cleaning up the manifests/site.pp file

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,22 +5,20 @@
 # import 'nodes.pp'
 
 # This file is managed by Puppet. Any manual changes will be lost.
-# This line added 2015-02-17 13:59
 
-# nodes.pp file
+# from the nodes.pp file
 
 node 'puppetDemo' {
 
-# From Puppet 3 Cookbook 
-  include puppet
+  include puppet      # From Puppet 3 Cookbook 
+  include nginx       # Page 38
+  include sudoers     # Page 82 - step 6
+  include ssh         # Page 80 - step 4
 
 # PAGE 34
 #  package { 'nginx':
 #    ensure => installed,
 #  }
-
-# PAGE 38
-  include nginx
 
 # PAGE 95
 #  file { '/var/www/cat-pictures':
@@ -46,12 +44,6 @@ node 'puppetDemo' {
 #  hour    => '04',
 #  minute  => '00',
 # }
-
-# PAGE 82 - step 6
-  include sudoers
-
-# PAGE 80 - step 4
-  include ssh
 
 # PAGE 73
 #  user { 'art':
@@ -82,9 +74,6 @@ node 'puppetDemo' {
 #    type => 'rsa',
 #    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
 #  }
-
-
-
 
 }
 
