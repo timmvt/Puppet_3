9ee4f4188ec76d15ac210de56141aa322e8f7ef9
timmvt <timmvt@gmail.com>
2015-02-20 14:38:28 -0500

    Added code from page 98 to work on cat-pictures.com

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,70 +9,60 @@
 # from the nodes.pp file
 
 node 'puppetDemo' {
-
   include puppet      # From Puppet 3 Cookbook 
+
   include nginx       # Page 38
-  include sudoers     # Page 82 - step 6
+  $site_name = 'cat-pictures' # Page 98
+  $site_domain = 'cat-pictures.com'
+  file { '/etc/nginx/sites-enabled/cat-pictures.conf':
+    content => template('nginx/vhost.conf.erb'),
+    notify  => Service['nginx'],
+  }
+
+#  include sudoers     # Page 82 - step 6
 #  include ssh         # Page 80 - step 4
 
-# PAGE 34
-#  package { 'nginx':
+#  package { 'nginx': # PAGE 34
 #    ensure => installed,
 #  }
 
-# PAGE 95
-#  file { '/var/www/cat-pictures':
-#    ensure => directory,
-#  }
-
-#  file { '/var/www/cat-pictures/img':
-#    source => 'puppet:///modules/cat-pictures/img',
-#    recurse => true,
-#    require => File['/var/www/cat-pictures'],
-#  }
-
-# PAGE 94 - CHAPTER 6 EXERCISE
-  cron { 'run pull-updates from Git':
+  cron { 'run pull-updates from Git': # PAGE 94 - CHAPTER 6 EXERCISE
     command => '/usr/bin/rsync -az /usr/local/bin/pull-updates',
     hour    => '*',
     minute  => '5',
   }
 
-# PAGE 92 Scheduling a backup
-# cron { 'Back up cat-pictures':
-#  command => '/usr/bin/rsync -az /var/www/cat-pictures/ /cat-pictures-backup/',
-#  hour    => '04',
-#  minute  => '00',
-# }
+  cron { 'Back up cat-pictures': # PAGE 92 Scheduling a backup
+    command => '/usr/bin/rsync -az /var/www/cat-pictures/ /cat-pictures-backup/',
+    hour    => '04',
+    minute  => '00',
+  }
 
-# PAGE 73
-#  user { 'art':
+  file { '/var/www/cat-pictures': # PAGE 95
+    ensure => directory,
+  }
+
+  file { '/var/www/cat-pictures/img':
+    source => 'puppet:///modules/cat-pictures/img',
+    recurse => true,
+    require => File['/var/www/cat-pictures'],
+  }
+
+
+#  user { 'art': # PAGE 73
 #    ensure => present,
 #    comment => 'Art Vandalay',
 #    home => '/home/art',
 #    managehome => true,
 #  }
-
-# PAGE 77
-#  ssh_authorized_key { 'art_ssh':
+#  ssh_authorized_key { 'art_ssh': # PAGE 77
 #    user => 'art',
 #    type => 'rsa',
 #    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
 #  }
 
-# PAGE 73
-#  user { 'testProfile':
-#    ensure => present,
-#    comment => 'test profile',
-#    home => '/home/test',
-#    managehome => true,
-#  }
-
-# PAGE 77
-#  ssh_authorized_key { 'testProfile_ssh':
-#    user => 'testProfile',
-#    type => 'rsa',
-#    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFBoLkAAkVcTsIOZnIYaS7NKzQebiqGLuGG9hZyYLd8ZZF6tPWCHatGqoLjhC/W0MaO/BGA4lPWQxZK4ifz+4+eoPZkX3xyVj1GIxyrxrdkIeI2qkw7QHupfjIogz7jFHpmWZT9HJU4Irqbt9SRRKl3/mD2Rp1kl06oRhjP4nTENP8RucgD33ymaPMY+pdUSyIA7igv8nKO+j0JOEmqJPldqC83ax0rVtaWqa9oO1qvpOA67x6grWFObnIF4WyqN/C+RBqzh4pJHzO7h1Cgt2FijKxEgAcafvj+mBE7plCLazsyWJ+ic+vrwwhniczn4tYScjFTLzUVqNzOfPra0S7'
+#  exec { 'Run my arbitrary command':
+#    command => '/bin/echo I ran this command on `/bin/date` >/tmp/command.output.txt',
 #  }
 
 }
